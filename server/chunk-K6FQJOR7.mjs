0f765a7c1634b6c12f31e71887dab6e97e217125
John Mercado <jmercado@bitconsultingusa.net>
2025-07-15 17:59:21 -0400

    Auto-generated commit

--- server/chunk-K6FQJOR7.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{Ma as o,Na as a,Xa as i,ya as n}from"./chunk-FMCPU5AE.mjs";import"./chunk-T55IDOPT.mjs";var c=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-creative"]],decls:2,vars:0,template:function(e,p){e&1&&(o(0,"p"),i(1,"creative works!"),a())},encapsulation:2,changeDetection:0})};export{c as CreativeComponent};
--- server/chunk-LKOL7PRE.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{Ma as a,Na as o,Xa as r,ya as n}from"./chunk-FMCPU5AE.mjs";import"./chunk-T55IDOPT.mjs";var i=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-barberia"]],decls:2,vars:0,template:function(e,c){e&1&&(a(0,"p"),r(1,"barberia works!"),o())},encapsulation:2,changeDetection:0})};export{i as BarberiaComponent};
